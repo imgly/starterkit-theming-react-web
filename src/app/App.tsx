285e9f43d3e82407eed4031774068916f5660323
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-16 14:49:22 +0200

    Update starterkit for v1.82.1-rc.0

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,6 +14,7 @@ import { ThemingSidebar } from './ThemingSidebar';
 
 import styles from './App.module.css';
 
+
 interface AppProps {
   editorConfig: Configuration;
 }
@@ -32,7 +33,7 @@ export function App({ editorConfig }: AppProps) {
     // Scene Loading
     // ============================================================================
 
-    await instance.loadFromURL(resolveAssetPath('/assets/example-1.scene'));
+    await instance.load(resolveAssetPath('/assets/example-1.scene'));
 
     setCesdk(instance);
   }, []);
